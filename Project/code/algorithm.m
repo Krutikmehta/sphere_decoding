613ea56bee0cd8443c7a4d0914136f9f0b6be260
Krutik Mehta <pkrutikmehta@gmail.com>
2020-08-01 12:34:08 +0530

    both

--- Project/code/algorithm.m
+++ Project/code/algorithm.m
@@ -7,52 +7,55 @@ global x_est; x_est= [];
 global d_c;d_c = 2;
 global m; m=4;
 global i; i=m;
+global Q, Q=4;
+global R; R=[1,2,1,2;0,1,2,1;0,0,3,1;0,0,0,2];
+y = [1,2,3,1];
+bounds(x,A,B,T,E,x_est,d_c, m,i,Q,R,y);
 
-
-function bounds(y,E,d_c,T,i,R)
+function bounds(x,A,B,T,E,x_est,d_c,m,i,Q,R,y)
     if d_c < T(i)
-        increment(i,m);
+        increment(x,A,B,T,E,x_est,d_c, m,i,Q,R,y);
     else
         compute1= (y(i)-E(i)-sqrt(d_c-T(i)))/R(i,i);
         compute2= (y(i)-E(i)+sqrt(d_c-T(i)))/R(i,i);
         A(i)=max(0,ceil(compute1));
         B(i)=min(Q-1,floor(compute2));
         x(i)=A(i)-1;
-        natural_spanning(x,B,i);
+        natural_spanning(x,A,B,T,E,x_est,d_c, m,i,Q,R,y);
     end
 end
 
 
-function natural_spanning(x,B,i)
+function natural_spanning(x,A,B,T,E,x_est,d_c, m,i,Q,R,y)
     x(i)=x(i)+1;
     if x(i) <= B(i)
-        decrement(R,x,T,E,y,i);      
+        decrement(x,A,B,T,E,x_est,d_c, m,i,Q,R,y);      
     else
-        increment(i,m);
+        increment(x,A,B,T,E,x_est,d_c, m,i,Q,R,y);
     end
 end
 
 
 
-function increment(i,m)
+function increment(x,A,B,T,E,x_est,d_c, m,i,Q,R,y)
     if i==m
         return 
     else
         i=i+1;
-        natural_spanning(x,B,i);
+        natural_spanning(x,A,B,T,E,x_est,d_c, m,i,Q,R,y);
     end
 end
 
 
 
-function decrement(R,x,T,E,y,i)
+function decrement(x,A,B,T,E,x_est,d_c, m,i,Q,R,y)
     if i>1
         for j = i:m
             E(i-1) = E(i-1) + R(i-1,j)*x(j);
         end
         T(i-1) = T(i) + (y(i)-E(i)-R(i,i)*x(i))^2;
         i = i-1;
-        bounds(y,E,d_c,T,i,R);
+        bounds(x,A,B,T,E,x_est,d_c, m,i,Q,R,y);
         
     else
         x_est = [x_est x];
@@ -62,7 +65,7 @@ function decrement(R,x,T,E,y,i)
             for L = 1:m
                 B(L) = min(Q-1,floor(y(L)-E(L)+sqrt(d_c-T(L))/R(L,L)));
             end
-        natural_spanning(x,B,i);
+        natural_spanning(x,A,B,T,E,x_est,d_c, m,i,Q,R,y);
         
         end
     
